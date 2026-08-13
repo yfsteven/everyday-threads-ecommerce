9adfbe47585af02142c5cb41d8df89a7235b9c15
Fahad Haider <haiderfahad078078@gmail.com>
2026-08-13 17:24:39 -0400

    Fix order route ordering

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,9 +5,10 @@ const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
 
 router.post('/', verifyToken, orderController.createOrder);
 router.get('/my-orders', verifyToken, orderController.getUserOrders);
-router.get('/:id', verifyToken, orderController.getOrderById);
 
 router.get('/admin/all', verifyToken, isAdmin, orderController.getAllOrders);
 router.patch('/:id/status', verifyToken, isAdmin, orderController.updateOrderStatus);
+
+router.get('/:id', verifyToken, orderController.getOrderById);
 
 module.exports = router;
